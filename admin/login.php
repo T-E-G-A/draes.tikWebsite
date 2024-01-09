c62ebffba5dad5eb3fc76882d50a8394d2b2148c
T-E-G-A <104275659+T-E-G-A@users.noreply.github.com>
2024-01-09 03:19:22 +0100

    added front-end of login page

---
+++ admin/login.php
@@ -0,0 +1,33 @@
+<html>
+   <head>
+       <title>Login draes.tik Admin System</title>
+       <link rel="stylesheet" href="../css/admin.css">
+       
+   </head>
+   <body>
+   <div class="center">
+      <h1 style>Login</h1>
+
+      <!-- Login form starts here -->
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <input type="submit" value="Login" class="btn-LP">
+        <!-- Login form ends here -->
+
+        <div class="author_link">
+          <p class="text-center">Created By <a href="#">Edward Oviasogie</a></p>
+        </div>
+      </form>
+    </div>
+      
+   </body>
+</html>
